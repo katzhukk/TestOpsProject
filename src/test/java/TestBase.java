09bbb5206fc863cdabd25797f22bca69248bc498
katzhukk <katzhukova230@gmail.com>
2024-11-08 23:50:25 +0300

    update TestBase.java

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,2 +1,31 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
 public class TestBase {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.browserSize = System.getProperty("browserSize");
+        Configuration.browser = System.getProperty("browser");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.remote = System.getProperty("urlHost");
+        Configuration.baseUrl = "https://www.gazprombank.ru/";
+        Configuration.pageLoadStrategy = "eager";
+        //Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 10000;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
 }
